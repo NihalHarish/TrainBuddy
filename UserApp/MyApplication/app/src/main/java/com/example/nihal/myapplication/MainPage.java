2615cd5185cc67ffbb3af4420cc45cb5bd23ebe1
NihalHarish <nihal42harish@gmail.com>
2015-04-18 22:18:50 +0530

    Check!

---
+++ UserApp/MyApplication/app/src/main/java/com/example/nihal/myapplication/MainPage.java
@@ -0,0 +1,20 @@
+package com.example.nihal.myapplication;
+
+import android.os.Bundle;
+
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+
+/**
+ * Created by nihal on 18/4/15.
+ */
+
+
+
+public class MainPage extends MaterialNavigationDrawer {
+
+     @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+
+}
